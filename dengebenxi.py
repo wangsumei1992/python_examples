c8b371a5789c23d91fd17a3ddff018399f3d2c50
578380132@qq.com <wq15803863660@>
2019-01-06 18:21:14 +0800

    dengebenxi

--- dengebenxi.py
+++ dengebenxi.py
@@ -1,17 +1,19 @@
 class DengeBenXi:
+    def __init__(self, account, term, rate, jiaxi=0, youhuiquan=0):
+        self.account = account
+        self.term = term
+        self.rate = rate
+        self.jiaxi = jiaxi
+        self.youhuiquan = youhuiquan
 
-	def __init__(self,account, term, rate, jiaxi=0, youhuiquan=0):
-		self.account = float(account)
-		self.term = int(term)
-		self.rate = float(rate)
-		self.jiaxi = float(jiaxi)
-		self.youhuiquan = float(youhuiquan)
+    def changgui_biao(self):
+        # 每月应付金额 = 贷款本金×[年化利率/12×(1+年化利率/12) ^ 还款月数]÷ {[(1+年化利率/12) ^ 还款月数]-1}
+        #a = self.account * (self.rate / 12) * [(1 + self.rate / 12) ** self.term] / [(1 + self.rate / 12) ** self.term - 1]
+        a=(1+self.rate/12)**self.term*(self.rate/12)
+        b=(1+self.rate/12)**self.term-1
+        c=self.account*a/b
+        return c
 
-
-	def changgui_biao(self):
-		# 每月应付金额 = 贷款本金×[年化利率/12×(1+年化利率/12) ^ 还款月数]÷ {[(1+年化利率/12) ^ 还款月数]-1}
-		return self.account * {(self.rate / 12) * [(1 + self.rate / 12) ** self.term]} / [(1 + self.rate / 12) ** self.term - 1]
 
 mini = DengeBenXi(39000, 12, 0.105)
-mini.changgui_biao()
-
+print(mini.changgui_biao())
